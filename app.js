f79ae87964cc7386885869f9c999041741d4925e
Hardbuoy <alhassanhakim5@gmail.com>
2024-09-01 14:26:15 +0000

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hard Buoy Records Amalitech AWS Restart Portfolio Project'));
+app.get('/', (req, res) => res.send('Hard Buoy Records Amalitech AWS Restart Portfolio Project Finally Wrapping up'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
